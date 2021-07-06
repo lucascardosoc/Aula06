af6c7d27eb6afe6a498d5f6632c48bd6c33d741a
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-07-05 22:30:49 -0300

    Vetores

---
+++ src/EntendendoVetores.java
@@ -0,0 +1,25 @@
+
+public class EntendendoVetores {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner 
+		//Criação de um vetor
+		int vetor[] = new int[20];
+		
+		vetor[0] = 50;
+		vetor[1] = 30;
+		vetor[2] = 70;
+		vetor[3] = 90;
+		vetor[4] = 70;
+	
+		
+		
+		for(int i=0;i<20;i++) {
+		}
+		System.out.println(vetor[2]);
+
+	}
+
+}
